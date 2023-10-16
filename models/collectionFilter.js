5f04689e66d7c9f2c78fc3222cc049a24ca16b3d
WillyNilly44 <94925679+WillyNilly44@users.noreply.github.com>
2023-10-16 09:37:19 -0400

    Update collectionFilter.js

--- models/collectionFilter.js
+++ models/collectionFilter.js
@@ -8,13 +8,70 @@ export default class CollectionFilter {
     }
     get()
     {
+        let newObjects = [];
         if(this.params == null)
         {
             return this.objectList
         }
-        //type = this.params[0]
-       // if(type=='sort'){
-            //sort
-       // }
+        let type = Object.keys(this.params)[0]
+        let value = this.params[type]
+
+        console.log(value.substring(0,value.length-1))
+
+
+        if(this.model.isMember(type))
+        {
+            if(value.startsWith('*') && value.endsWith('*'))
+            {
+                let newValue = value.substring(1,value.length-1);
+                for(let filtered of this.objectList)
+                {
+                    if(filtered[type].includes(newValue))
+                    {
+                        newObjects.push(filtered);
+                    }
+                }; 
+                return newObjects;
+            }
+            else if(value.startsWith('*'))
+            {
+                let newValue = value.substring(1);
+                for(let filtered of this.objectList)
+                {
+                    if(filtered[type].endsWith(newValue))
+                    {
+                        newObjects.push(filtered);
+                    }
+                };
+                return newObjects; 
+            }
+            else if(value.endsWith('*'))
+            {
+                let newValue = value.substring(0,value.length-1);
+                for(let filtered of this.objectList)
+                {
+                    if(filtered[type].startsWith(newValue))
+                    {
+                        newObjects.push(filtered);
+                    }
+                };
+                return newObjects;
+            }
+            else
+            {
+                for(let filtered of this.objectList)
+                {
+                    if(filtered[type] == value)
+                    {
+                        newObjects.push(filtered);
+                    }
+                };
+                return newObjects;
+            }
+            
+            
+            
+        }
+       
     }
 }
