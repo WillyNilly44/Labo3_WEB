3dfc36edf8111b6d1be3201aa4fdbfe2378dfdf6
WillyNilly44 <94925679+WillyNilly44@users.noreply.github.com>
2023-10-16 19:08:46 -0400

    Update collectionFilter.js

--- models/collectionFilter.js
+++ models/collectionFilter.js
@@ -1,77 +1,64 @@
-
-
 export default class CollectionFilter {
     constructor(objectList, params, model) {
         this.objectList = objectList;
         this.params = params;
         this.model = model;
     }
-    get()
-    {
+    get() {
         let newObjects = [];
-        if(this.params == null)
-        {
+        if (this.params == null) {
             return this.objectList
         }
         let type = Object.keys(this.params)[0]
         let value = this.params[type]
 
-        console.log(value.substring(0,value.length-1))
+        console.log()
 
-
-        if(this.model.isMember(type))
+        if(type == 'sort')
         {
-            if(value.startsWith('*') && value.endsWith('*'))
-            {
-                let newValue = value.substring(1,value.length-1);
-                for(let filtered of this.objectList)
-                {
-                    if(filtered[type].includes(newValue))
-                    {
-                        newObjects.push(filtered);
-                    }
-                }; 
-                return newObjects;
-            }
-            else if(value.startsWith('*'))
-            {
-                let newValue = value.substring(1);
-                for(let filtered of this.objectList)
-                {
-                    if(filtered[type].endsWith(newValue))
-                    {
-                        newObjects.push(filtered);
-                    }
-                };
-                return newObjects; 
-            }
-            else if(value.endsWith('*'))
-            {
-                let newValue = value.substring(0,value.length-1);
-                for(let filtered of this.objectList)
-                {
-                    if(filtered[type].startsWith(newValue))
-                    {
-                        newObjects.push(filtered);
-                    }
-                };
-                return newObjects;
-            }
-            else
-            {
-                for(let filtered of this.objectList)
-                {
-                    if(filtered[type] == value)
-                    {
-                        newObjects.push(filtered);
-                    }
-                };
-                return newObjects;
-            }
-            
-            
             
         }
-       
+
+        if (this.model.isMember(type)) {
+            if (value.startsWith('*') && value.endsWith('*')) {
+                newValue = value.substring(1, value.length - 1);
+                for (let filtered of this.objectList) {
+                    if (filtered[type].includes(newValue)) {
+                        newObjects.push(filtered);
+                    }
+                };
+                return newObjects;
+            }
+            else if (value.startsWith('*')) {
+                let newValue = value.substring(1);
+                for (let filtered of this.objectList) {
+                    if (filtered[type].endsWith(newValue)) {
+                        newObjects.push(filtered);
+                    }
+                };
+                return newObjects;
+            }
+            else if (value.endsWith('*')) {
+                let newValue = value.substring(0, value.length - 1);
+                for (let filtered of this.objectList) {
+                    if (filtered[type].startsWith(newValue)) {
+                        newObjects.push(filtered);
+                    }
+                };
+                return newObjects;
+            }
+            else {
+                for (let filtered of this.objectList) {
+                    if (filtered[type] == value) {
+                        newObjects.push(filtered);
+                    }
+                };
+                return newObjects;
+            }
+
+
+
+        }
+
     }
 }
