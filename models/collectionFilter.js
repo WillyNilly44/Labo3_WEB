855d1c370dae30ede984cf238e8af441baec86f6
WillyNilly44 <94925679+WillyNilly44@users.noreply.github.com>
2023-10-20 12:06:56 -0400

    Update collectionFilter.js

--- models/collectionFilter.js
+++ models/collectionFilter.js
@@ -6,22 +6,44 @@ export default class CollectionFilter {
     }
     get() {
         let newObjects = [];
+        let type;
+        let type2;
+        let value;
+        let offset;
+        let limit;
+
         if (this.params == null) {
             return this.objectList
         }
-        let type = Object.keys(this.params)[0]
-        let value = this.params[type]
-
-        console.log()
-
-        if(type == 'sort')
-        {
-            this.objectList.sort(function(a,b){
-                return innerCompare(a[value],b[value])
+        if (Object.keys(this.params).length == 1) {
+            type = Object.keys(this.params)[0];
+            value = this.params[type]
+        }
+        else if (Object.keys(this.params).length == 2) {
+            type = Object.keys(this.params)[0];
+            type2 = Object.keys(this.params)[1];
+            limit = this.params[type];
+            offset = this.params[type2];
+        }
+        //console.log()
+        if (type2 != undefined) {
+            if (type == 'limit' && type2 == 'offset') {
+                let index = parseInt(offset);
+                let newList = [];
+                let nogo = parseInt(limit) + parseInt(offset);
+                while (index < nogo) {
+                    newList.push(this.objectList[index])
+                    index++;
+                }
+                return newList;
+            }
+        }
+        if (type == 'sort') {
+            this.objectList.sort(function (a, b) {
+                return innerCompare(a[value], b[value])
             })
             return this.objectList;
         }
-
         if (this.model.isMember(type)) {
             if (value.startsWith('*') && value.endsWith('*')) {
                 newValue = value.substring(1, value.length - 1);
@@ -64,22 +86,27 @@ export default class CollectionFilter {
         }
 
     }
-    
+
 }
-compareNum(x,y)
-{
-    if (x === y) {return 0;}
-    else if(x < y){return -1;}
+function compareNum(x, y) {
+    if (x === y) { return 0; }
+    else if (x < y) { return -1; }
     return 1;
 }
-innerCompare(x, y)
-{
-    if ((typeof x) === 'string')
-    {
+function innerCompare(x, y) {
+    if ((typeof x) === 'string') {
         return x.localeCompare(y);
     }
-    else
-    {
+    else {
         return compareNum(x, y);
     }
-} 
+}
+function valueMatch(value, searchValue) {
+    try {
+        let exp = '^' + searchValue.toLowerCase().replace(/\*/g, '.*') + '$';
+        return new RegExp(exp).test(value.toString().toLowerCase());
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
